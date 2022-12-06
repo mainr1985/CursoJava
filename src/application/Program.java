1c78ea901c3d8090f78d5e19fa75e1ff6c8d70ee
Maíra N. Rocha <mairanrocha@hotmail.com>
2022-12-05 22:06:21 -0300

    Novo método findByDepartment adicionado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -9,7 +12,11 @@ public class Program {
 	public static void main(String[] args) {
 				
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(1);			
-		System.out.println(seller);	
+		//Seller seller = sellerDao.findById(1);	
+		Department dep = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller obj:list) { //forEach
+			System.out.println(obj);	
+		}			
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -93,4 +96,46 @@ public class SellerDaoJDBC implements SellerDao {
 		
 		return null;
 	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st= conn.prepareStatement (	"   SELECT seller.*, department.Name as DepName "+
+										"   FROM seller "+
+												" INNER JOIN department ON seller.DepartmentId = department.Id "
+										+ " WHERE DepartmentId = ? "
+										+ " ORDER BY Name ");
+			
+			st.setInt(1, department.getId()); //o id que entra como parâmetro na função. Explica quem é o id que a query quer, e que é um dado de tipo Integer
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList();
+
+			//estrutura chave-valor, a chave é um integer (id do departamento) e o valor é um objeto do tipo Department. Está sendo usado Map pra garantir não repetição de departamentos
+			Map<Integer,Department> map = new HashMap(); //guarda departamentos nesse caso
+
+			//percorrendo o resultset - testando se traz algum resultado. O resultset inicialmente aponta pra posição 0, que não possui dados. Só tem dado a partir da posição 1.
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep==null) { //só instancia o departamento se for nulo
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); //guarda o departamento no map
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);		
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}	
+	}	
 }
