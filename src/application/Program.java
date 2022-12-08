1cc444599104f3503fed5cdf4cd1c0ae9e5479c5
Maíra N. Rocha <mairanrocha@hotmail.com>
2022-12-07 21:13:55 -0300

    métodos Update e Delete implementados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,7 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 				
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		//Seller seller = sellerDao.findById(1);	
+		Seller seller = sellerDao.findById(1);	
 		Department dep = new Department(2,null);
 		//List<Seller> list = sellerDao.findByDepartment(dep);
 		List<Seller> list = sellerDao.findAll();
@@ -22,7 +23,19 @@ public class Program {
 		}			
 		
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,dep);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted. New id = " + newSeller.getId());
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Maria Luna");
+		sellerDao.update(seller);
+		System.out.println("Update realizado");
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Informe um usuário:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete realizado");
+		sc.close();
 	}
 }
